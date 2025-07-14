69c23e306ce7b75ca5901621050dd368e91c313e
Akil Tajwar <akiltajwar@gmail.com>
2025-07-14 17:01:47 +0600

    wip: transaction report api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import authRoutes from "./auth.routes";
 import companyRoutes from "./company.routes"
 import accountMainRoutes from "./account-main.routes";
 import transactionRoutes from "./transaction.routes";
+import reportRoutes from "./reports.routes";
 const router=Router()
 
 router.use('/auth',authRoutes)
 router.use('/company',companyRoutes)
 router.use('/account-main',accountMainRoutes)
 router.use('/transaction',transactionRoutes)
+router.use('/report',reportRoutes)
 
 export default router;
